1db465aa3208a0a588cb349e917c38b264408fc3
rozoxmere <pawloq0078@gmail.com>
2022-10-12 14:15:46 +0200

    migrations and added django debug toolbar

--- apka_www/settings.py
+++ apka_www/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
 
 ROOT_URLCONF = 'apka_www.urls'
@@ -122,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(blank=True, null=True, max_length=255)
+    description = models.TextField(blank=True, null=True)
+    amount = models.IntegerField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    rating = models.FloatField(blank=True, null=True)
+    class Meta:
+        managed = False
+        db_table = 'product'
