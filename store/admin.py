b8791f21440bae9c35aadfe95bc24a81217f78ca
rozoxmere <pawloq0078@gmail.com>
2022-10-19 13:19:21 +0200

    zad 1-3

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from store.models import Product
+from store.models import Product, Osoba
 
 # Register your models here.
 
 admin.site.register(Product)
+admin.site.register(Osoba)
--- store/models.py
+++ store/models.py
@@ -10,3 +10,23 @@ class Product(models.Model):
     class Meta:
         managed = False
         db_table = 'product'
+
+class Osoba(models.Model):
+    MONTHS = (
+        ("1", "Styczen"),
+        ("2", "Luty"),
+        ("3", "Marzec"),
+        ("4", "Kwiecien"),
+        ("5", "Maj"),
+        ("6", "Czerwiec"),
+        ("7", "Lipiec"),
+        ("8", "Sierpien"),
+        ("9", "Wrzesien"),
+        ("10", "Pazdziernik"),
+        ("11", "Listopad"),
+        ("12", "Grudzien"),
+    )
+    imie = models.TextField(null=False, blank=False)
+    nazwisko = models.TextField(null=False, blank=False)
+    miesiac_urodzenia = models.CharField(max_length=2, choices=MONTHS, default=(MONTHS[0]))
+    data_dodania = models.DateField(auto_now_add=True)
